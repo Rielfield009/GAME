a62ff9a074e1cbaea9d6e68975f3e5b9332ebd2d
Jeanky23 <116356015+Jeanky23@users.noreply.github.com>
2022-12-19 21:33:23 -0300

    Update game.py

--- game.py
+++ game.py
@@ -1,3 +1,7 @@
+import random
+#numero_aleatorio = random.randint(1, 10) #para obtener un número entero aleatorio entre 1 y 10, puedes utilizar la función randint
+#función random() para obtener un número aleatorio entre 0 y 1
+#función uniform(a, b) para obtener un número aleatorio flotante entre a y b
 class Character:
     def __init__(self,hp=1000,attack=200,defense=300,magic=100):
         self._hp = hp
@@ -7,6 +11,30 @@ class Character:
 
     def welcome(self):
         print("Bienvenido jugador selecciona un pj")
+
+    def set_hp(self, hp):                           #sets and gets
+        self.__hp = hp
+    
+    def get_hp(self):
+        return self.__hp
+
+    def set_attck(self, attack):
+        self.__attack = attack
+    
+    def get_attack(self):
+        return self.__attack
+    
+    def set_defense(self, defense):
+        self.__defense = defense
+    
+    def get_defense(self):
+        return self.__defense
+    
+    def set_magic(self, magic):
+        self.__magic = magic
+    
+    def get_magic(self):
+        return self.__magic
     
 class Wizard(Character):
     def __init__(self,hp=1000,attack=200,defense=300,magic=100,wisdom=500):
